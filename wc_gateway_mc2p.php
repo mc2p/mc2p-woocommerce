a7bef34a2f6bd2237772852cd0ea6261ef90386e
Abel Puentes Luberta <apuentes@mychoice2pay.com>
2022-07-21 12:25:29 +0200

    Improved iframe and updated version

--- wc_gateway_mc2p.php
+++ wc_gateway_mc2p.php
@@ -5,7 +5,7 @@
  * Description: WooCommerce library for the MyChoice2Pay API.
  * Author: MyChoice2Pay
  * Author URI: https://www.mychoice2pay.com/
- * Version: 1.3.0
+ * Version: 1.3.1
  * Text Domain: wc_mc2p_payment_gateway
  * Domain Path: /i18n/languages/
  *
@@ -61,7 +61,7 @@ add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'wc_mc2p_gatew
  *
  * @class 		WC_Gateway_MC2P
  * @extends		WC_Payment_Gateway
- * @version		1.3.0
+ * @version		1.3.1
  * @package		WooCommerce/Classes/Payment
  * @author 		MyChoice2Pay
  */
@@ -331,7 +331,7 @@ function wc_mc2p_gateway_init() {
             }
 
             if ( $this->way == $this->WAY_IFRAME ) {
-				echo '<iframe src="'.$obj->getIframeUrl().'" frameBorder="0" style="width: 100%; height: 700px"></iframe>';
+				echo '<iframe allowpaymentrequest sandbox="allow-forms allow-popups allow-pointer-lock allow-same-origin allow-scripts allow-top-navigation" src="'.$obj->getIframeUrl().'" frameBorder="0" style="width: 100%; height: 700px"></iframe>';
             } else {
 				return array(
 					'result' 	=> 'success',
@@ -396,7 +396,7 @@ function wc_mc2p_gateway_init() {
                         "billing_postal_code" => $postal_code,
                         "mc2p_lib" => array(
                             "name" => "woocommerce",
-                            "version" => "1.3.0"
+                            "version" => "1.3.1"
                         )
 					)
                 )
@@ -488,7 +488,7 @@ function wc_mc2p_gateway_init() {
                         "billing_postal_code" => $postal_code,
                         "mc2p_lib" => array(
                             "name" => "woocommerce",
-                            "version" => "1.3.0"
+                            "version" => "1.3.1"
                         )
 					)
                 )
